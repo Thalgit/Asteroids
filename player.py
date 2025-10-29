dd775e855deb8c0aee99f2c4bf957ee351fb9128
Thalgit <steeladres@outlook.com>
2025-10-29 19:43:04 +0100

    bewegen

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -8,6 +8,7 @@ class Player(CircleShape):
 		self.rotation = 0
 
 	#player lijkt driehoek, is stiekem cirkel
+
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
 		right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -19,6 +20,8 @@ class Player(CircleShape):
 	def draw(self, screen):
 		pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+	# draaien en bewegen
+
 	def rotate(self, dt):
 		self.rotation += PLAYER_TURN_SPEED * dt
 
@@ -29,3 +32,11 @@ class Player(CircleShape):
 			self.rotate(-dt)
 		if keys[pygame.K_d]:
 			self.rotate(dt)
+		if keys[pygame.K_w]:
+			self.move(dt)
+		if keys[pygame.K_s]:
+			self.move(-dt)
+
+	def move(self, dt):
+		forward = pygame.Vector2(0, 1).rotate(self.rotation)
+		self.position += forward * PLAYER_SPEED * dt

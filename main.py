b16f4803deb43b5bc789fd41c071070a11dfa67a
Thalgit <steeladres@outlook.com>
2025-10-29 15:49:56 +0100

    player driehoek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -12,14 +13,18 @@ def main():
     dt = 0
    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
    
     #raam sluit = stop program
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         #60 fps cap 
